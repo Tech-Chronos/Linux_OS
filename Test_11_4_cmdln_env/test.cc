f2b82ac2ffceddc33d8700d4fc93e7d22e85b141
有趣的中国人 <dongsijie8@gmail.com>
2025-11-04 12:02:56 +0800

    命令行参数 & 环境变量

--- Test_11_4_cmdln_env/test.cc
+++ Test_11_4_cmdln_env/test.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
 #include <unistd.h>
 #include <sys/types.h>
 #include <cstring>
@@ -7,6 +8,24 @@
 using namespace std;
 
 int g_val = 100000;
+
+// 本质是bash中的环境变量可以继承给子进程，就是bash中维护了env的表（全局的)
+// 然后子进程可以看到
+int main(int argc, char* argv/*, char* environ[]*/)
+{
+    extern char** environ;
+    for (int i = 0; environ[i]; ++i)
+    {
+        cout << "env[" << i << "] : " << environ[i] << endl;
+    }
+
+    cout << "----------------------------------" << endl;
+
+    const char* str = getenv("PATH");
+
+    cout << str << endl;
+}
+
 
 // 父进程的数据子进程能看到
 // int main()
@@ -29,34 +48,34 @@ int g_val = 100000;
 
 
 // 命令行参数：可根据在命令行的输入定制结果
-int main(int argc, char* argv[])
-{
-    if (argc != 2)
-    {
-        cout << "usage method -> " << argv[0] << "-[a b c d]" << endl;
-    }
-    else if (strcmp(argv[1], "-a") == 0)
-    {
-        cout << "func A : " << endl;
-    }
-    else if (strcmp(argv[1], "-b") == 0)
-    {
-        cout << "func B : " << endl;
-    }
-    else if (strcmp(argv[1], "-c") == 0)
-    {
-        cout << "func C : " << endl;
-    }
-    else if (strcmp(argv[1], "-d") == 0)
-    {
-        cout << "func D : " << endl;
-    }
-    else
-    {
-        cout << "we don't have this function!!!" << endl;
-    }
-    return 0;
-}
+// int main(int argc, char* argv[])
+// {
+//     if (argc != 2)
+//     {
+//         cout << "usage method -> " << argv[0] << "-[a b c d]" << endl;
+//     }
+//     else if (strcmp(argv[1], "-a") == 0)
+//     {
+//         cout << "func A : " << endl;
+//     }
+//     else if (strcmp(argv[1], "-b") == 0)
+//     {
+//         cout << "func B : " << endl;
+//     }
+//     else if (strcmp(argv[1], "-c") == 0)
+//     {
+//         cout << "func C : " << endl;
+//     }
+//     else if (strcmp(argv[1], "-d") == 0)
+//     {
+//         cout << "func D : " << endl;
+//     }
+//     else
+//     {
+//         cout << "we don't have this function!!!" << endl;
+//     }
+//     return 0;
+// }
 
 // 本质是命令行输入的参数首先被bash拿到，而且在命令行中执行的可执行文件都是bash的子进程
 // 所以命令行的参数在bash中被解析成argv，然后bash用fork()创建子进程，调用execv()
