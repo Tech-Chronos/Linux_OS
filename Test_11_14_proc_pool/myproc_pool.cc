97161ca8d2d769e90d918846e666d6ffdf3747c3
有趣的中国人 <dongsijie8@gmail.com>
2025-11-14 19:38:30 +0800

    进程池

---
+++ Test_11_14_proc_pool/myproc_pool.cc
@@ -0,0 +1,76 @@
+#include "process_pool.hpp"
+
+/// @brief 父进程创建信道的函数
+/// @param channels 外部传递的信道，vector保存
+/// @param id 用来指知道子进程id
+/// @param pipenum 管道数量
+void CreateChannel(std::vector<Channel> &channels, pid_t *id, int pipenum)
+{
+    int pipefd[2] = {0};
+    for (int i = 0; i < pipenum; ++i)
+    {
+        pipe(pipefd);
+        // 4. 子进程获取信道读文件描述符
+        *id = fork();
+
+        if (*id == 0)
+        {
+            // 子进程关闭写文件描述符
+            close(pipefd[1]);
+
+            // 7. 子进程执行任务
+            // 7.1 首先需要获取任务的id
+            int taskid = 0;
+            GetFuncIdAndExcute(&taskid, pipefd[0]);
+            // 7.2 然后再执行
+            //ExcuteTask(taskid);
+
+            // 子进程执行完直接退出
+            exit(0);
+        }
+        else
+        {
+            // 现在父进程已经知道了子进程的id和写文件描述符
+            channels.emplace_back(pipefd[1], *id);
+
+            // 5. 父进程关闭读文件描述符
+            close(pipefd[0]);
+
+            std::cout << pipefd[1] << std::endl;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    // 1. 命令行输入要几个管道的数量
+    int pipenum = 0;
+    if (argc == 1)
+    {
+        std::cout << "usage method -> " << argv[0] << " [PIPENUMBER]" << std::endl;
+        exit(1);
+    }
+    else
+    {
+        pipenum = std::stoi(argv[1]);
+    }
+
+    // 2. 管理信道，装载任务
+    std::vector<Channel> channels;
+
+    LoadTask();
+
+    // 3. 父进程创建管道，
+    // 先创建 id
+    pid_t id = 0;
+
+    CreateChannel(channels, &id, pipenum);
+
+    // 6. 向信道中分配任务
+    // 先分配五次任务
+    AllocTask(channels, 5);
+
+    CompleteAndQuit(channels);
+
+    return 0;
+}
---
+++ Test_11_14_proc_pool/process_pool.hpp
@@ -0,0 +1,167 @@
+#ifndef PROCESS_POOL
+
+#define PROCESS_POOL
+#ifdef PROCESS_POOL
+
+#include <iostream>
+#include <vector>
+#include <functional>
+
+#include <cstdlib>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define TASK_SIZE 3
+
+std::function<void()> func[TASK_SIZE];
+
+class Channel
+{
+public:
+    Channel() = default;
+
+    Channel(int wfd, int childprocid)
+        : _wfd(wfd), _childprocid(childprocid)
+    {
+    }
+
+    int GetWfd()
+    {
+        return _wfd;
+    }
+
+    pid_t GetChildprocid()
+    {
+        return _childprocid;
+    }
+
+    // 当父进程写fd关闭，子进程就会知道，就会推出
+    void CloseWfd()
+    {
+        int ret = close(_wfd);
+        if (ret == 0)
+        {
+            std::cout << "close success !" << std::endl;
+        }
+    }
+
+    void WaitChild()
+    {
+        int rid = waitpid(_childprocid, nullptr, 0);
+        if (rid == 0)
+        {
+            std::cout << "child quit success !" << std::endl;
+        }
+    }
+
+private:
+    int _wfd;
+    pid_t _childprocid;
+};
+
+class task
+{
+public:
+    static void print_task()
+    {
+        std::cout << "I am a print_task !" << std::endl;
+    }
+
+    static void load_task()
+    {
+        std::cout << "I am a load_task !" << std::endl;
+    }
+
+    static void sql_task()
+    {
+        std::cout << "I am a sql_task !" << std::endl;
+    }
+};
+
+// 装载任务
+void LoadTask()
+{
+    srand(time(nullptr) ^ getpid() ^ 7777);
+
+    func[0] = std::bind(&task::print_task);
+    func[1] = std::bind(&task::load_task);
+    func[2] = std::bind(&task::sql_task);
+}
+
+// 分配一次的函数
+void AllocTaskOnce(std::vector<Channel> &channels)
+{
+    sleep(1);
+    int taskid = random() % TASK_SIZE;
+
+    for (int i = 0; i < channels.size(); ++i)
+    {
+        // std::cout << i << " -> "<< taskid << std::endl;
+        int writefd = channels[i].GetWfd();
+        // 面向字节流
+        int ret = write(writefd, &taskid, sizeof(taskid));
+        if (ret < 0)
+        {
+            std::cerr << "write error!" << std::endl;
+        }
+    }
+}
+
+// 父进程分配任务，可以传递次数
+void AllocTask(std::vector<Channel> &channels, int times = -1)
+{
+    if (times > 0)
+    {
+        while (times--)
+        {
+            AllocTaskOnce(channels);
+        }
+    }
+    else
+    {
+        while (true)
+        {
+            AllocTaskOnce(channels);
+        }
+    }
+}
+
+// 子进程执行任务
+void ExcuteTask(int taskid)
+{
+    std::cout << "I am a worker, my id = " << getpid() << ", I am doing task " << taskid << std::endl;
+    func[taskid]();
+}
+
+// 获取任务ID
+void GetFuncIdAndExcute(int *taskid, int rfd)
+{
+    while (true)
+    {
+        int ret = read(rfd, taskid, sizeof(taskid));
+        if (ret < 0)
+        {
+            std::cerr << "read error !" << std::endl;
+        }
+        ExcuteTask(*taskid);
+    }
+}
+
+
+
+void CompleteAndQuit(std::vector<Channel> &channels)
+{
+    for (auto &channel : channels)
+    {
+        channel.CloseWfd();
+    }
+    for (auto &channel : channels)
+    {
+        channel.WaitChild();
+    }
+}
+
+#endif
+#endif
