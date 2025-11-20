c49caea565e13030a826cc5ea6681b526ef42b23
有趣的中国人 <dongsijie8@gmail.com>
2025-11-20 15:20:07 +0800

    信号的保存

---
+++ Test_11_20_signal/signal.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+
+#include <unistd.h>
+#include <signal.h>
+
+void PrintPending(sigset_t pending_set)
+{
+    std::cout << "my pid is [" << getpid() << "], pending table is ->";
+    for (int i = 31; i > 0; --i)
+    {
+        // 判断 i 号信号是否在 pending 位图中
+        if (sigismember(&pending_set, i))
+        {
+            std::cout << "1";
+        }
+        else
+        {
+            std::cout << "0";
+        }
+    }
+    std::cout << std::endl;
+}
+
+void handler(int signo)
+{
+    std::cout << signo << std::endl;
+}
+
+// 信号的保存
+int main()
+{
+    // 0. 2号信号自定义为handler
+    //signal(2, handler);
+    // 1. 定义两个位图
+    sigset_t block_set, old_set;
+
+    // 2. 清空位图
+    sigemptyset(&block_set);
+    sigemptyset(&old_set);
+
+    // 3. 设置信号（2信号被阻塞）
+    sigaddset(&block_set, 2);
+
+    // 4. 发送给进程
+    sigprocmask(SIG_BLOCK, &block_set, &old_set);
+
+    sigset_t pending_set;
+    sigemptyset(&pending_set);
+
+    int cnt = 5;
+    while (true)
+    {
+        // 5. 打印pending
+        sigpending(&pending_set);
+        PrintPending(pending_set);
+        sleep(1);
+
+        --cnt;
+        if (!cnt)
+        {
+            sigprocmask(SIG_UNBLOCK, &block_set, nullptr);
+        }
+    }
+
+    return 0;
+}
